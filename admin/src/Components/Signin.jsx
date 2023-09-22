1048c06a64ccd48492322830d3f1ad5ebb1c8708
sejal99 <sejal8974@gmail.com>
2023-09-22 22:34:35 +0530

    first commit

--- admin/src/Components/Signin.jsx
+++ admin/src/Components/Signin.jsx
@@ -1,31 +1,80 @@
 import React, { useState } from 'react'
 import axios from 'axios'
-const Signin = () => {
-    const [useremail,setuseremail]=useState('')
-    console.log(useremail);
-    const [userpassword,setuserpassword]=useState('')
-    console.log(userpassword);
-  return (
-    <div>
-        <h2>Signin</h2>
-      <form style={{alignItems:'center'}}>
-        {/* <label for="email">email</label> */}
-        <input placeholder='your email' onChange={(e)=>setuseremail(e.target.value)} />
-        {/* <label for="password">password</label> */}
-        <input type='password'placeholder='your password'  onChange={(e)=>setuserpassword(e.target.value)} />
-        <button onClick={async()=>{
-            try {
-              const response= await axios.post('http://localhost:3000/admin/login',{username:useremail,password:userpassword})
-              localStorage.setItem('token',response.data.token);
-              alert('user loggedin successfully');
-              console.log(response);
-            } catch (error) {
-               console.log(error); 
-            }
-        }}>Signin</button>
-      </form>
-    </div>
-  )
-}
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import {Card, Typography} from "@mui/material";
 
-export default Signin
+const Signin = () => {
+    const [email,setEmail]=useState('')
+    console.log(email);
+    const [password,setPassword]=useState('')
+    console.log(password);
+   
+ 
+  
+      return <div>
+              <div style={{
+                  paddingTop: 150,
+                  marginBottom: 10,
+                  display: "flex",
+                  justifyContent: "center"
+              }}>
+                  <Typography variant={"h6"}>
+                  Welcome Back! Sign in below
+                  </Typography>
+              </div>
+          <div style={{display: "flex", justifyContent: "center"}}>
+              <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+                  <TextField
+                      onChange={(evant11) => {
+                          let elemt = evant11.target;
+                          setEmail(elemt.value);
+                      }}
+                      fullWidth={true}
+                      label="Email"
+                      variant="outlined"
+                  />
+                  <br/><br/>
+                  <TextField
+                      onChange={(e) => {
+                          setPassword(e.target.value);
+                      }}
+                      fullWidth={true}
+                      label="Password"
+                      variant="outlined"
+                      type={"password"}
+                  />
+                  <br/><br/>
+  
+                  <Button
+                      size={"large"}
+                      variant="contained"
+                      onClick={async () => {
+                          const res = await axios.post(`${BASE_URL}/admin/login`, {
+                              username: email,
+                              password: password
+                          },
+                          //  {
+                          //     headers: {
+                          //         "Content-type": "application/json"
+                          //     }
+                          // }
+                          );
+                          const data = res.data;
+  
+                          localStorage.setItem("token", data.token);
+                          // window.location = "/"
+                          setUser({
+                              userEmail: email,
+                              isLoading: false
+                          })
+                          navigate("/courses")
+                      }}
+  
+                  > Signin</Button>
+              </Card>
+          </div>
+      </div>
+  }
+  
+  export default Signin;
