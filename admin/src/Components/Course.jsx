b643c4c71cb83afba68795c522ff8e649e7de77d
sejal99 <sejal8974@gmail.com>
2023-09-24 23:45:17 +0530

    neww commit

--- admin/src/Components/Course.jsx
+++ admin/src/Components/Course.jsx
@@ -2,29 +2,26 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { BASE_URL } from "../../config";
 import { useParams } from "react-router-dom";
-import { Card, TextField,Button } from "@mui/material";
+import { Card, TextField, Button } from "@mui/material";
 const Course = () => {
-    
-    const [title, setTitle]=useState("");
-    
-    const [description, setDescription]=useState("");
-    const [price, setPrice]=useState(0);
-    const [image, setImage]=useState("");
+  const [title, setTitle] = useState("");
 
-    const {courseId}=useParams()
-    console.log(courseId);
-    
-     useEffect(() => {
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [image, setImage] = useState("");
 
+  const { courseId } = useParams();
+  console.log(courseId);
+
+  useEffect(() => {
     const func = async () => {
       try {
         const res = await axios.get(`${BASE_URL}/admin/course/${courseId}`, {
-
           headers: {
             Authorization: "bearer " + localStorage.getItem("token"),
           },
         });
-        
+
         setTitle(res.data.course.title);
         setDescription(res.data.course.description);
         setImage(res.data.course.image);
@@ -36,89 +33,120 @@ const Course = () => {
     func();
   }, []);
 
- 
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        minHeight: "80vh",
+        flexDirection: "column",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card
+          variant={"outlined"}
+          style={{ width: 400, padding: 20, marginTop: 30, height: "100%" }}
+        >
+          <TextField
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            fullWidth={true}
+            label="Title"
+            variant="outlined"
+          />
 
-  return <div
-   style={{display: "flex", 
-    justifyContent: "center", 
-    minHeight: "80vh",  
-    flexDirection: "column"}}>
+          <TextField
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+            fullWidth={true}
+            label="Description"
+            variant="outlined"
+          />
 
-        <div
-         style={{display: "flex", 
-         justifyContent: "center"}}>
+          <TextField
+            value={image}
+            onChange={(e) => {
+              setImage(e.target.value);
+            }}
+            fullWidth={true}
+            label="Image link"
+            variant="outlined"
+          />
 
-            <Card 
-            variant={"outlined"} 
-            style={{width: 400, padding: 20, marginTop: 30, height: "100%"}}>
+          <TextField
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            fullWidth={true}
+            label="Price"
+            variant="outlined"
+          />
 
+          <Button
+            size={"large"}
+            variant="contained"
+            onClick={async () => {
+              await axios.put(
+                `${BASE_URL}/admin/courses/` + courseId,
+                {
+                  title: title,
+                  description: description,
+                  imageLink: image,
+                  published: true,
+                  price,
+                },
+                {
+                  headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token"),
+                  },
+                }
+              );
+              alert("updated course!");
+            }}
+          >
+            {" "}
+            Update course
+          </Button>
+            {" "}
+          <Button
+            variant="contained"
+             size={"large"}
 
-                <TextField
-                  value={title}
-                    onChange={(e)=>{
-                        setTitle(e.target.value);
-                    }}
-                 
-                    fullWidth={true}
-                    label="Title"
-                    variant="outlined"
-                />
+            onClick={async () => {
+             
+              const confirmDelete = window.confirm(
+                "Are you sure you want to delete this item?"
+              );
+              if (confirmDelete) {
+                try {
+                  await axios.delete(
+                    `${BASE_URL}/admin/course/` + courseId,
 
-                <TextField
-                 value={description}
-                      onChange={(e)=>{
-                        setDescription(e.target.value);
-                    }}
-                       
-                    fullWidth={true}
-                    label="Description"
-                    variant="outlined"
-                />
-
-                <TextField
-                    value={image}
-                      onChange={(e)=>{
-                        setImage(e.target.value);
-                    }}
-                
-                    fullWidth={true}
-                    label="Image link"
-                    variant="outlined"
-                />
-
-                <TextField
-                   value={price}
-                 onChange={(e)=>{
-                    setPrice(e.target.value);
-                }}
-              
-                    fullWidth={true}
-                    label="Price"
-                    variant="outlined"
-                />
-
-                <Button
-                    size={"large"}
-                    variant="contained"
-                    onClick={async () => {
-                        await axios.put(`${BASE_URL}/admin/courses/`+ courseId , {
-                            title: title,
-                                description: description,
-                                imageLink: image,
-                                published: true,
-                                price
-                        }, {
-                            headers: {
-                                "Authorization": "Bearer " + localStorage.getItem("token")
-                            }
-                        });
-                        alert("updated course!");
-                    }}
-                > Update course</Button>
-            </Card>
-        </div>
-
-  </div>
+                    {
+                      headers: {
+                        Authorization: `Bearer ${localStorage.getItem(
+                          "token"
+                        )}`,
+                      },
+                    }
+                  );
+                } catch (error) {
+                  console.error("Error deleting item:", error);
+                }
+              }
+            }}
+          >
+            Delete Course
+          </Button>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default Course;
