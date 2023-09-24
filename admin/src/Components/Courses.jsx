3cf97867e52e89d4ff82f0a6976bf3835ba98d15
sejal99 <sejal8974@gmail.com>
2023-09-24 19:57:06 +0530

    new commit

--- admin/src/Components/Courses.jsx
+++ admin/src/Components/Courses.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import { Button, Card, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom";
 import { BASE_URL } from "../../config";
 const Courses = () => {
   const [courses, setCourses] = useState([]);
@@ -19,40 +21,38 @@ const Courses = () => {
       }
     };
     fun();
-
   }, []);
 
   console.log(courses);
 
-  return(
-    <div>
-        {courses.map((course)=>(
-            <div>
-                <div>
-                   { course.title}
-                    
-                </div>
-                <div>
-                    {course.description}
-                </div>
-
-                <img src= {course.imageLink}/>
-                   
-                
-
-
-                <div>
-                    {course.price}
-                </div>
-
-            </div>
-        )
-
+  return <div style={{display: "flex", flexWrap: "wrap", justifyContent: "center"}}>
+        {courses.map(course => {
+            return <Course course={course} />}
         )}
     </div>
-  )
+}
 
-  
-};
+export function Course({course}) {
+    const navigate = useNavigate();
+
+    return <Card style={{
+        margin: 10,
+        width: 300,
+        minHeight: 200,
+        padding: 20
+    }}>
+        <Typography textAlign={"center"} variant="h5">{course.title}</Typography>
+        <br/>
+        <Typography textAlign={"center"} variant="subtitle1">{course.description}</Typography>
+        <br/>
+        <img src={course.imageLink} style={{width: 300}} ></img>
+        <div style={{display: "flex", justifyContent: "center", marginTop: 20}}>
+            <Button variant="contained" size="large" onClick={() => {
+                navigate("/course/" + course._id);
+            }}>Edit</Button>
+        </div>
+    </Card>
+
+}
 
 export default Courses;
